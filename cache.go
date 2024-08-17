c25ee44ce42c12e296952e39cf8f0d83dfa4b5a2
zhanglp0129 <zhanglp0129@gmail.com>
2024-08-17 23:03:44 +0800

    优化redis查询代码

--- cache.go
+++ cache.go
@@ -3,16 +3,19 @@ package redis_cache
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"github.com/redis/go-redis/v9"
 	"math/rand"
 	"time"
 )
 
+type QueryFunc[T any] func() (T, error)
+
 // QueryWithCache 带着缓存查询
 // model，传出参数，数据模型
 // query，查询函数，缓存未命中时调用
 // return，是否命中缓存
-func QueryWithCache[T any](rdb redis.UniversalClient, key string, model *T, query func() (*T, error), options ...Option) (bool, error) {
+func QueryWithCache[T any](rdb redis.UniversalClient, key string, model *T, query QueryFunc[*T], options ...Option) (bool, error) {
 	// 获取缓存配置
 	config := CacheConfig{
 		cacheTime: getDefaultCacheTime(),
@@ -22,18 +25,24 @@ func QueryWithCache[T any](rdb redis.UniversalClient, key string, model *T, quer
 		opt(&config)
 	}
 
-	if n, err := rdb.Exists(config.ctx, key).Result(); err == nil && n > 0 {
+	data, err := rdb.Get(config.ctx, key).Result()
+	if err == nil {
 		// 命中缓存
-		return true, readFromRedis(rdb, key, model, &config)
+		err = json.Unmarshal([]byte(data), model)
+		if err != nil {
+			// json解析出错，以未命中缓存处理
+			return false, cacheMiss(rdb, key, model, query, &config)
+		}
+
+		// 刷新缓存时间
+		return true, rdb.Expire(config.ctx, key, config.cacheTime).Err()
+	} else if errors.Is(err, redis.Nil) {
+		// 未命中缓存
+		return false, cacheMiss(rdb, key, model, query, &config)
 	}
 
-	// 未命中缓存
-	data, err := query()
-	if err != nil {
-		return false, err
-	}
-	*model = *data
-	return false, writeToRedis(rdb, key, model, &config)
+	// 出现错误
+	return false, err
 }
 
 // 获取默认缓存时间
@@ -41,32 +50,20 @@ func getDefaultCacheTime() time.Duration {
 	return time.Duration(55+rand.Int()%11) * time.Second
 }
 
-// 从redis读取数据，并保存在model中
-func readFromRedis[T any](rdb redis.UniversalClient, key string, model *T, cfg *CacheConfig) error {
-	data, err := rdb.Get(cfg.ctx, key).Result()
+// 缓存未命中
+func cacheMiss[T any](rdb redis.UniversalClient, key string, model *T, query QueryFunc[*T], config *CacheConfig) error {
+	res, err := query()
 	if err != nil {
 		return err
 	}
+	*model = *res
 
-	err = json.Unmarshal([]byte(data), model)
-	if err != nil {
-		return err
-	}
-
-	// 刷新缓存时间
-	rdb.Expire(cfg.ctx, key, cfg.cacheTime)
-
-	return nil
-}
-
-// 将model的数据写入redis
-func writeToRedis[T any](rdb redis.UniversalClient, key string, model *T, cfg *CacheConfig) error {
 	data, err := json.Marshal(model)
 	if err != nil {
 		return err
 	}
 
-	return rdb.SetEx(cfg.ctx, key, string(data), cfg.cacheTime).Err()
+	return rdb.SetEx(config.ctx, key, string(data), config.cacheTime).Err()
 }
 
 // DeleteCache 删除缓存
